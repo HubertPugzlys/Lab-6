f0963854bdc92f8e2e3cfc31e6c9a5fb51be6f49
Hubertgit123 <phubert.pugzlys@ufl.edu>
2023-03-20 17:18:39 -0400

    Final version of 1st member final

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
         elif user_selection == 2:
-            print(f'The encoded password is {encoded_password}, and the original password is {password}.')
+            print(f'The encoded password is {encoded_password}, and the original password is {password}.\n')
         else:
             break
 
